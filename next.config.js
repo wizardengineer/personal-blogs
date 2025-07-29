e6bd4a9d57192fdd6f9d4c51e74e9713fd717a89
wizardengineer <juliuswoosebert@gmail.com>
2025-07-29 00:00:20 -0400

    testing again...for bug

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+import withMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withMDX({
+  pageExtensions: ["js","jsx","ts","tsx","md","mdx"],
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeHighlight],
+  },
+  // ← Tell Next (and the action) “yes, use static export”
+  output: "export",
+});
+
+export default nextConfig;
