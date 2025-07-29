a78ad6a0e0654be17912d69686c84e8371ca591b
wizardengineer <juliuswoosebert@gmail.com>
2025-07-29 01:03:23 -0400

    another test

--- next.config.cjs
+++ next.config.cjs
@@ -1,17 +1,32 @@
 // next.config.cjs
-const withMDX        = require("@next/mdx")({ extension: /\.mdx?$/ });
-const remarkGfm      = require("remark-gfm");
+const createMDX     = require("@next/mdx");
+const remarkGfm     = require("remark-gfm");
 const rehypeHighlight = require("rehype-highlight");
+
+let assetPrefix = "";
+let basePath   = "";
+
+if (process.env.GITHUB_ACTIONS) {
+  const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
+  assetPrefix = `/${repo}/`;
+  basePath   = `/${repo}`;
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  assetPrefix,
+  basePath,
+  images: { unoptimized: true },
   pageExtensions: ["js","jsx","ts","tsx","md","mdx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypeHighlight],
   },
-  output: "export",
-  images: { unoptimized: true },
-};
+});
 
 module.exports = withMDX(nextConfig);
