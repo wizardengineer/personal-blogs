4881cc5f4bc80c177c807a36c771a9f9f42a1439
wizardengineer <juliuswoosebert@gmail.com>
2025-07-29 09:04:44 -0400

    Fixing more issues

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+// next.config.js
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
+    output: 'export',
+   images: { unoptimized: true },
+}
+
+const withMDX = nextMDX({
+    extension: /\.(md|mdx)$/,
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeHighlight],
+    },
+});
+
+export default withMDX(nextConfig);
